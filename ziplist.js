9422330c350f0ba5fb59a4b71a216d90c826ebf7
Yeji Han <hany7@hawaii.edu>
2020-09-21 22:52:27 -1000

    finished the exercise2

--- ziplist.js
+++ ziplist.js
@@ -12,7 +12,7 @@ function zipList(list1, list2) {
 console.log(zipList(testList1, testList2));
 
 function zipListTheSimpleWay(list1, list2) {
-   return _.flatten(_.zip(list1, list2));
+  return _.flatten(_.zip(list1, list2));
 }
 
 console.log(zipListTheSimpleWay(testList1, testList2));
